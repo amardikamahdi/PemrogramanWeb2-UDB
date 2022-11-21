a1a6d849b48538494b25effc26d15e4a7bd597a4
pussy-cats <amardikamahdi@gmail.com>
2022-11-22 00:14:11 +0700

    fixing project structure, and separating style

--- index.php
+++ index.php
@@ -10,65 +10,7 @@ $query = mysqli_query($conn, "SELECT * FROM students");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <style>
-                     ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: #333;
-}
-
-li {
-  float: left;
-}
-
-li a {
-  display: block;
-  color: white;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-}
-
-/* Change the link color to #111 (black) on hover */
-li a:hover {
-  background-color: #111;
-}
-
-#customers {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
-
-#customers td, #customers th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#customers tr:nth-child(even){background-color: #f2f2f2;}
-
-#customers tr:hover {background-color: #ddd;}
-
-#customers th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #4c4f69;
-  color: white;
-  text-align: center;
-}
-.button {
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-}
-    </style>
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
     <?php include('components/navbar.php') ?>
@@ -85,7 +27,7 @@ li a:hover {
     <td><?php echo $data['nis']; ?></td>
     <td><?php echo $data['name']; ?></td>
     <td><?php echo $data['address']; ?></td>
-    <td><a class="button" style="background-color: #4CAF50" href="edit.php?id=<?php echo $data['id']; ?>">Edit</a></td>
+    <td><a class="button" style="background-color: #4CAF50" href="pages/students/edit.php?id=<?php echo $data['id']; ?>">Edit</a></td>
     <td><a class="button" style="background-color: #F44336" href="funcs/delete.php?id=<?php echo $data['id']; ?>">Delete</a></td>
   </tr>
   <?php } ?>
